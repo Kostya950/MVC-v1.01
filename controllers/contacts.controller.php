8153caa69a7701a5404c41856e7482ef6eae146b
Kostya Kovalchuk <Kostya950@gmail.com>
2016-05-22 21:41:54 +0300

    12. Sessions

--- controllers/contacts.controller.php
+++ controllers/contacts.controller.php
@@ -9,8 +9,18 @@
 class ContactsController extends Controller
 {
 
+    public function __construct($data=array())
+    {
+        parent::__construct($data);
+        $this->model = new Message();
+    }
+
     public function index()
     {
-
+      if ($_POST) {
+          if ($this->model->save($_POST)) {
+              Session::setFlash('Than you! Your message was sent successfully');
+          }
+      }
     }
 }
