a035e417df60c4ca87a0ea1be88f34279eb753d1
Kostya Kovalchuk <Kostya950@gmail.com>
2016-05-22 20:32:11 +0300

    9. Added Bootstrap

---
+++ controllers/contacts.controller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kito
+ * Date: 22.05.16
+ * Time: 20:25
+ */
+
+class ContactsController extends Controller
+{
+
+    public function index()
+    {
+
+    }
+}
